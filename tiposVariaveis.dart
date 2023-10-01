2f1051a292955360e22a083e34cda0c2c449383a
Veni <44931124+Venicode@users.noreply.github.com>
2023-10-01 13:31:53 -0300

    Add files via upload

---
+++ tiposVariaveis.dart
@@ -0,0 +1,7 @@
+void main() {
+  var nome = "Vitoria";
+  int idade = 19;
+  double altura = 1.65;
+  bool pessoa = (idade !=altura);
+  print("Meu nome é $nome, eu tenho $idade e minha altura é $altura. Pessoa: $pessoa");
+}
