b764e8cd0727e6e3a7210b5196f6e34736935192
venicode <vitoria_aparecida2011@hotmail.com>
2023-11-27 16:19:58 -0300

    new course about the exceptions

--- Prática/Paradigma OO/9 - Paciente/main.dart
+++ Prática/Paradigma OO/9 - Paciente/main.dart
@@ -2,7 +2,7 @@ import 'classConsulta.dart';
 import 'classPaciente.dart';
 import 'dart:io';
 void main(){
-  Map<Paciente,Consulta> agendamentos = {};
+  Map<String,Consulta> agendamentos = {};
   while (true) {
     Consulta novaConsulta = Consulta();
     print("Olá, para agendar uma consulta, por favor informe seu nome abaixo:\n");
@@ -11,29 +11,29 @@ void main(){
     int idade = int.parse(stdin.readLineSync()!);
     Paciente novoPaciente = Paciente(Nome: nome, Idade: idade, Consultas: novaConsulta);
     novaConsulta.AddNovaConsulta();
-    agendamentos[novoPaciente] = novaConsulta;
+    agendamentos[novoPaciente.Nome] = novaConsulta;
     novoPaciente.ExibirHistoricoConsultas();
 
     print("Deseja agendar mais consultas ou visualizar o histórico de agendamentos?"
         "\nDigite 1 para sim e qualquer tecla para não");
     int escolhaOpcao = int.parse(stdin.readLineSync()!);
     switch (escolhaOpcao){
-      case == 1:
+      case 1:
         print("Deseja agendar novas consultas(1) ou visualizar o histórico?(2)");
         int escolhaOpcao1 = int.parse(stdin.readLineSync()!);
         switch (escolhaOpcao1){
-          case == 1:
+          case 1:
             continue;
             break;
-          case == 2:
-            agendamentos.forEach((paciente, consulta) {
-              print("Agendamentos do $paciente:");
-              print(consulta);
+          case 2:
+            print(agendamentos.keys);
+            agendamentos.values.forEach((element) {
+              print(element);
             });
             break;
         }
         continue;
-      case == 2:
+      case 2:
         print("Certo, até logo");
         break;
     }
