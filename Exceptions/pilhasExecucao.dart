487b1e7e897493f03a7ffb778a88a71ad337078c
venicode <vitoria_aparecida2011@hotmail.com>
2023-11-29 17:39:45 -0300

    new course about the exceptions

---
+++ Exceptions/pilhasExecucao.dart
@@ -0,0 +1,19 @@
+void main(){
+  print("Started Main");
+  functionOne();
+  print("Finished Main");
+}
+void functionOne(){
+  print("Started F01");
+  functionTwo();
+  print("Finished F01");
+}
+void functionTwo(){
+  print("Started F02");
+  for (int i = 1; i <=5; i++){
+    print(i);
+    double amount = double.parse("Not a number");
+  }
+  print("Finished F02");
+}
+//Código de saida 0: encerrou sem nenhum erro
